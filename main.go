a93a95007e8a3feca7d06f650999bbae713e4d0f
notsatan <git@notsatan.me>
2022-01-25 00:29:24 +0530

    Update: Define package `src` to be an interface for package `main`
    This commit splits the existing code in package `main`, moving a
    large portion of the same to `src/src.go` instead
    
    The package `src` will act as an interface between the package
    `main` and the rest of the application. Currently, the plan is
    to have the pacakge expose a single public function
    
        func Run() error
    
    The function internally handles the pre and post setup scenarios
    needed before passing the flow-of-control to the root command, for
    example, reading environment variable(s) to switch to debug mode
    
    In case of an internal error, the function will wrap the error
    and simply return it back to the calling function - that being
    the function `main`
    
    This commit also improves tests by covering edge cases, such as
    ensuring logs aren't being written to log file in debug mode,
    logging isn't being enabled for production mode, etc
    
    If an error is returned, the function `main` will simply log
    the error and abort with a non-zero exit code to indicate failure
    
    Signed-off-by: notsatan <git@notsatan.me>

--- main.go
+++ main.go
@@ -3,60 +3,23 @@ package main
 import (
 	"os"
 
+	"github.com/notsatan/crcgen/src"
 	"github.com/notsatan/crcgen/src/logger"
 )
 
-const (
-	pkgName   = "main"
-	debugMode = "debug_mode"
-)
+const pkgName = "main"
 
 var (
-	// initLogger maps calls to logger.Log
-	initLogger = logger.Log
+	// run maps to src.Run
+	run = src.Run
 
 	// exit maps calls to os.Exit
 	exit = os.Exit
-
-	// closeLogger maps calls to logger.Stop
-	closeLogger = logger.Stop
 )
 
 func main() {
-	defer closeRes()
-
-	if _, ok := os.LookupEnv(debugMode); ok {
-		// In `debug` mode, switch logger to write all logs to `stderr`
-		err := initLogger(false)
-		crashOnErr(err, "Failed to start logger")
-
-		logger.Debugf("(%s/main): Detected `debug` mode", pkgName)
-	}
-}
-
-/*
-crashOnErr is a simple helper function to ensure a "graceful" crash if the main thread
-encounters an error.
-
-The function prints an error message to the user, closes resources and force closes the
-application
-*/
-func crashOnErr(err error, cause string) {
-	if err == nil {
-		return
-	}
-
-	logger.Errorf("(%s/main): %s: %s", pkgName, cause, err)
-	closeRes() // close resources
-
-	exit(-10)
-}
-
-/*
-closeRes attempts to close resources, designed to be run before the app closes
-*/
-func closeRes() {
-	if err := closeLogger(); err != nil {
-		logger.Errorf("(%s/main): Failed to close logger: %s", pkgName, err)
+	if err := run(); err != nil {
+		logger.Errorf("(%s/main): %s: %s", pkgName, err)
+		exit(-10)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,73 +1,30 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/notsatan/crcgen/src/logger"
+	"github.com/notsatan/crcgen/src"
 )
-
-const (
-	envProd  = "production_mode"
-	envDebug = "debug_mode"
-	envBuild = "__BUILD_MODE__"
-)
-
-func resetEnv() {
-	// Unset all environment variables
-	_ = os.Unsetenv(envProd)
-	_ = os.Unsetenv(envDebug)
-	_ = os.Unsetenv(envBuild)
-}
 
 func reset() {
+	run = src.Run
+
 	exit = os.Exit
-	initLogger = logger.Log
-	closeLogger = logger.Stop
 }
 
-func TestMain(m *testing.M) {
-	// Run all tests, unset env variables, and exit
-	resetEnv()
+func TestMain_Fail(t *testing.T) {
+	// Should call `exit` if an error is encountered
 
-	// TODO: Figure out a better way than running tests in debug mode
-	_ = os.Setenv(debugMode, "debug")
-
-	val := m.Run()
-	resetEnv()
-	os.Exit(val)
-}
-
-func TestMainMethod(_ *testing.T) {
-	main()
-}
-
-func TestCrashOnErr(t *testing.T) {
-	defer reset()
+	reset()
 
 	calls := 0
-	exit = func(int) { calls++ } // increment `calls` when `exit` is run by `CrashOnErr`
+	exit = func(int) { calls++ }
+	run = func() error { return fmt.Errorf("(%s/TestMain_Fail): test error", pkgName) }
 
-	crashOnErr(nil, "") // `exit` should not be called without an error
-	assert.Equal(t, calls, 0)
-
-	crashOnErr(errors.New("test"), "test message")
-	assert.Equal(t, calls, 1)
-}
-
-func TestCloseRes(t *testing.T) {
-	defer reset()
-
-	// `closeRes` is designed to run as an independent function - it should consume
-	// errors internally
-
-	closeLogger = func() error { return nil }
-	assert.NotPanics(t, func() { closeRes() })
-
-	closeLogger = func() error { return fmt.Errorf("(%s/main): test error", pkgName) }
-	assert.NotPanics(t, func() { closeRes() })
+	main()
+	assert.Equal(t, 1, calls, "failed to force close on error")
 }
