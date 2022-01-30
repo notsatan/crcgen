970adae97a9b6273f05503f12952f8fb85b60bd7
notsatan <git@notsatan.me>
2022-01-30 23:21:18 +0530

    Update: Define function `lib.WalkPath` to walk directories
    This commits adds a generic function `lib.WalkPath` that traverses
    the root directory, iterating through all the files under it.
    
    The function is a wrapper over `filepath.Walk` - taking a function
    as an input - this function will be run over each file present
    anywhere within the root directory.
        func WalkPath(path string, walkFunc filepath.WalkFunc) error
    
    The function `WalkPath` includes a custom check to validate the
    root directory, if the path is incorrect, an `errInvalidPath`
    error will be returned by the function
    
    `WalkPath` will act as the single flow-of-control regarding files
    read by `crcgen` - i.e. if files or directories are to ignored in
    the future, it will be as simple as modifying this function
    
    Signed-off-by: notsatan <git@notsatan.me>

--- src/lib/fs.go
+++ src/lib/fs.go
@@ -2,7 +2,9 @@ package lib
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/pkg/errors"
 )
@@ -11,6 +13,8 @@ const pkgName = "lib"
 
 // errInvalidPath indicates that the root path was invalid
 var errInvalidPath = fmt.Errorf("(%s): invalid path", pkgName)
+
+var filepathWalk = filepath.Walk
 
 /*
 IsInvalidPathErr indicates if an error was returned because of invalid input path
@@ -22,4 +26,27 @@ func IsInvalidPathErr(err error) bool {
 func PathExists(path string) bool {
 	_, err := os.Stat(path)
 	return err == nil
+}
+
+/*
+WalkPath walks through the root directory, running the input `walkFunc` function on all
+files. If the root path is invalid, a custom error is returned, use IsInvalidPathErr
+to check for this.
+
+Note: The parameter `walkFunc` will be selectively run on files
+*/
+func WalkPath(path string, walkFunc filepath.WalkFunc) error {
+	if !PathExists(path) {
+		return errInvalidPath
+	}
+
+	err := filepathWalk(path, func(path string, info fs.FileInfo, err error) error {
+		if err != nil || info.IsDir() {
+			return err
+		}
+
+		return walkFunc(path, info, err)
+	})
+
+	return errors.Wrapf(err, "(%s/WalkPath)", pkgName)
 }
