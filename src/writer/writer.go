097be77ea12940d5ab19c3f987d75ce67939f229
notsatan <git@notsatan.me>
2022-02-02 00:37:10 +0530

    Update: Define a base for package `src/writer`
    Introduce a basic structure for pacakge `src/writer` - with these
    changes, the package can be called by external packages
    
    The package introduces internal errors that other packages can
    check against - allowing detailed error messages for known errors.
    The private errors can be validated using the public functions;
        func IsInvalidFileErr(error) bool
        func IsInvalidExtErr(error) bool
    
    The central function `Start` acts as a wrapper around the private
    `writer.start` function. The public function ensures that the
    private function can be executed once at most
        func Start(string) error
    
    The function `writer.start` handles the following corner-cases;
     - Checks for valid file extensions
     - Extract file name from the full path
     - Extract file type from file extension in the path
     - Default the file type to "JSON" for files without an extension
     - Default to working directory if output directory is not specified
    
    On similar note, the function returns an error if;
     - Output file path does not contain a file name
     - File extension in the path is invalid (i.e. `.txt`, etc)
     - Viper is unable to locate the file in specified destinations
    
    Signed-off-by: notsatan <git@notsatan.me>

---
+++ src/writer/writer.go
@@ -0,0 +1,120 @@
+/*
+Package writer handles the part of writing output to a file
+
+Use the function Start to initialize the package
+*/
+package writer
+
+import (
+	"fmt"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+
+	"github.com/notsatan/crcgen/src/logger"
+)
+
+const pkgName = "writer"
+
+var once sync.Once
+
+// output is the central instance of `viper` being used by package `writer`
+var output = viper.New()
+
+var readViperConfigs = (*viper.Viper).ReadInConfig
+
+var (
+	errInvalidFile = fmt.Errorf("(%s): could not detect output file in path", pkgName)
+	errInvalidExt  = fmt.Errorf("(%s): output file has invalid extension", pkgName)
+)
+
+/*
+IsInvalidFileErr checks if an error returned by package writer was caused because the
+output file could not be located from the path
+*/
+func IsInvalidFileErr(err error) bool {
+	return errors.Is(err, errInvalidFile)
+}
+
+/*
+IsInvalidExtErr checks if an error returned by package `writer` was caused by the file
+having an invalid extension
+*/
+func IsInvalidExtErr(err error) bool {
+	return errors.Is(err, errInvalidExt)
+}
+
+/*
+Start initializes package `writer` - should be executed before calls to any function
+from this package
+
+Returns an error if the output file could not be parsed from the path, if the output
+file contains an invalid extension, or if function Start is being called multiple times
+Use the functions IsInvalidFileErr, and IsInvalidExtErr to explicitly check for these
+errors
+
+Note: This function can be run once
+*/
+func Start(confPath string) error {
+	var err error
+	once.Do(func() {
+		err = start(confPath)
+	})
+
+	return err
+}
+
+/*
+start initializes package `writer`, setting up the configurations needed
+*/
+func start(confPath string) error {
+	const logTag = "(" + pkgName + "/Start)"
+
+	// Split path to get directory, filename and extension (remove `dot` from ext)
+	dir, file := filepath.Split(confPath)
+	ext := strings.ToLower(strings.TrimLeft(filepath.Ext(file), "."))
+
+	if file == "" {
+		return errors.Wrap(errInvalidFile, logTag)
+	}
+
+	if ext == "" {
+		ext = "json"
+		logger.Warnf(
+			"%s: output file `%s` has no type - defaulting to JSON", logTag, file,
+		)
+	}
+
+	if ok := validateExt(ext); !ok {
+		logger.Errorf(`Directory, File, Extension: ("%v", "%v", "%v")`, dir, file, ext)
+		return errors.Wrap(errInvalidExt, logTag)
+	}
+
+	if dir == "" {
+		dir = "."
+		logger.Infof("%s: destination directory defaulted to work directory", logTag)
+	}
+
+	output.SetConfigType(ext)
+	output.AddConfigPath(dir)
+	output.SetConfigName(file)
+
+	err := readViperConfigs(output)
+	return errors.Wrap(err, logTag)
+}
+
+/*
+validateExt validates if an extension is valid for the output file
+*/
+func validateExt(ext string) bool {
+	switch strings.ToLower(ext) {
+	case "json", "yaml", "yml":
+		return true
+
+	default:
+		return false
+	}
+}
---
+++ src/writer/writer_test.go
@@ -0,0 +1,102 @@
+package writer
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+
+	"github.com/pkg/errors"
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+//nolint:deadcode,unused // keeping this for future use
+func reset() {
+	readViperConfigs = (*viper.Viper).ReadInConfig
+}
+
+func TestIsInvalidExtErr(t *testing.T) {
+	for err, expected := range map[error]bool{
+		nil:                      false,
+		errInvalidExt:            true,
+		errInvalidFile:           false,
+		fmt.Errorf("test error"): false,
+		fmt.Errorf(""):           false,
+		fmt.Errorf("(%s): output file has invalid extension", pkgName): false,
+	} {
+		assert.Equalf(
+			t, expected, IsInvalidExtErr(err),
+			`failed to match "%v" -> "%v"`, expected, err,
+		)
+	}
+}
+
+func TestIsInvalidFileErr(t *testing.T) {
+	for err, expected := range map[error]bool{
+		nil:                      false,
+		errInvalidExt:            false,
+		errInvalidFile:           true,
+		fmt.Errorf("test error"): false,
+		fmt.Errorf(""):           false,
+		fmt.Errorf("(%s): output file has invalid extension", pkgName): false,
+	} {
+		assert.Equalf(
+			t, expected, IsInvalidFileErr(err),
+			`failed to match "%v" -> "%v"`, expected, err,
+		)
+	}
+}
+
+func TestStart(t *testing.T) {
+	// Checks to ensure `Start` can be run exactly once
+	readViperConfigs = func(*viper.Viper) error { return nil }
+
+	once = sync.Once{} // reset to isolate this test
+
+	invalidInput := "/root/" // expect an error since no file is specified
+	assert.Errorf(
+		t, Start(invalidInput),
+		`expected failure for invalid input: "%s"`, invalidInput,
+	)
+
+	// On the second run, the function `Start` should return directly, and no error
+	// should be possible even for invalid input
+	assert.NoErrorf(t, Start(invalidInput), `function "Start" running multiple times`)
+}
+
+func TestInternalStart(t *testing.T) {
+	readViperConfigs = func(*viper.Viper) error { return nil }
+
+	for input, err := range map[string]error{
+		"":             errInvalidFile, // no file specified
+		"file.txt":     errInvalidExt,  // invalid extension
+		"/tmp/":        errInvalidFile, // no file specified
+		"/dest/file":   nil,            // extension should default to `JSON`
+		"config.YAML":  nil,            // default to working directory
+		"/file.yAML":   nil,            // case-insensitivity ensured
+		"/config.YmL":  nil,
+		"/config.JSon": nil,
+	} {
+		result := start(input)
+
+		if err == nil {
+			assert.NoErrorf(t, result, `unexpected error for input: "%s"`, input)
+		} else {
+			assert.Errorf(t, result, `no error for invalid input: "%s"`, input)
+			assert.True(t, errors.Is(result, err))
+		}
+	}
+}
+
+func TestValidateExt(t *testing.T) {
+	for input, expected := range map[string]bool{
+		"jSon":             true, // case-insensitive expected
+		"YaMl":             true,
+		"yML":              true,
+		"invalid input":    false,
+		"unexpected-input": false,
+	} {
+		result := validateExt(input)
+		assert.Equalf(t, expected, result, `check passed for invalid input "%s"`, input)
+	}
+}
