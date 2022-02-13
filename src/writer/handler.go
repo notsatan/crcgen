847c63813dbf6e9f2256d0cccd73d94060d90846
notsatan <git@notsatan.me>
2022-02-13 22:46:22 +0530

    Update: Define interface `writer.Handler`
    With issue #7, the current plan is to replace usage `viper` with
    the `encodings.json` package
    
    This commit adds a simple interface - `Handler` that defines the
    basic structure of any "Output file handler". Each handler defines
    an object that interacts with certain file types based on the
    output file extension
    
    Signed-off-by: notsatan <git@notsatan.me>

---
+++ src/writer/handler.go
@@ -0,0 +1,21 @@
+package writer
+
+/*
+Handler defines a simple interface to interact with data from the output file - this
+includes reading data from the file, and writing to the same file when done
+
+Each Handler defines an object that can interact with certain file types (based on file
+extensions)
+*/
+type Handler interface {
+	// Marshal takes an object of DirInfo as input, and converts it into a byte array
+	// that can be easily written to files
+	Marshal(*DirInfo) ([]byte, error)
+
+	// Unmarshal parses encoded data and stores the result in the DirInfo object
+	Unmarshal([]byte, *DirInfo) error
+
+	// FileTypes returns an array of strings - indicating the supported output file
+	// extensions. The extensions are case-insensitive
+	FileTypes() []string
+}
